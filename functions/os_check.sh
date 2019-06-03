4d2c3742cb20c143fa8b8846aa9503b371963fe8
marshki <marshall@morethantomorrow.org>
2019-06-03 08:21:24 -0400

    add os check

---
+++ functions/os_check.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash 
+# detect system architecture, then act
+  
+case $(uname -s) in
+Darwin)
+  printf "%s\\n" "Darwin"
+  ;;
+Linux)
+  printf "%s\\n" "Linux" 
+  ;;
+*)
+  printf "%s\\n" "He can't handle your speed, $(uname -s)"
+  ;;
+esac 
