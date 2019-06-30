83f4bcbfc9ac2f3a231d42dd7feacf341f6d7bb3
marshki <marshall@morethantomorrow.org>
2019-06-30 11:14:21 -0400

    update

--- functions/GNU_Linux/create_default_dirs.sh
+++ functions/GNU_Linux/create_default_dirs.sh
@@ -2,13 +2,16 @@
 # Create user directories, e.g. Desktop, Music, etc.
 # https://wiki.archlinux.org/index.php/XDG_user_directories
 
+username=bwilson
+
 create_default_dirs () { 
  
   if [[ ! -z $(which xdg-user-dirs-update) ]]
   then
   #su - ${username} -c "xdg-user-dirs-update"
   ### if you want to use sudo instead of su:
-  -u ${username} -i xdg-user-dirs-update
-  #sudo -u ${username} -i xdg-user-dirs-update
+  sudo -u ${username} -i xdg-user-dirs-update
   fi
 } 
+
+create_default_dirs
