35745558794af15246541f968c272c6c3a36752c
marshki <marshall@morethantomorrow.org>
2019-07-02 21:55:28 -0400

    add comments re: passing password

--- functions/GNU_Linux/create_default_dirs.sh
+++ functions/GNU_Linux/create_default_dirs.sh
@@ -2,6 +2,11 @@
 # Create user directories, e.g. Desktop, Music, etc.
 # https://wiki.archlinux.org/index.php/XDG_user_directories
 
+### TODO: 
+### Something like:
+# read -s -p "Enter sudo password: " passy
+# printf "%s" $passy |sudo -u ${username} -i ... 
+
 username=bwilson
 
 create_default_dirs () { 
@@ -10,7 +15,7 @@ create_default_dirs () {
   then
   #su - ${username} -c "xdg-user-dirs-update"
   ### if you want to use sudo instead of su:
-  sudo -u ${username} -i xdg-user-dirs-update
+  #sudo -u ${username} -i xdg-user-dirs-update
   fi
 } 
 
