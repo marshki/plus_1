2cc4cbfaac515ef1648e9b0148ed0169ee2bb4e9
marshki <marshall@morethantomorrow.org>
2019-07-05 10:31:44 -0400

    rm stale comments

--- functions/GNU_Linux/create_default_dirs.sh
+++ functions/GNU_Linux/create_default_dirs.sh
@@ -2,22 +2,7 @@
 # Create user directories, e.g. Desktop, Music, etc.
 # https://wiki.archlinux.org/index.php/XDG_user_directories
 
-### TODO: 
-### Something like:
-# read -s -p "Enter sudo password: " passy
-# printf "%s" $passy |sudo -u ${username} -i ... 
-
-#create_default_dirs () { 
- 
-#  if [[ ! -z $(which xdg-user-dirs-update) ]]
-#  then
-  #su - ${username} -c "xdg-user-dirs-update"
-  ### if you want to use sudo instead of su:
-  #sudo -u ${username} -i xdg-user-dirs-update
-#  fi
-#} 
-
-username='estyron'
+username='sjobs'
 
 create_default_dirs () { 
  
