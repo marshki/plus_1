e8672acbdb560ba2b657c7f12d30acd814ba743a
marshki <marshall@morethantomorrow.org>
2019-07-05 06:57:39 -0400

    rewrite function

--- functions/GNU_Linux/create_default_dirs.sh
+++ functions/GNU_Linux/create_default_dirs.sh
@@ -7,8 +7,6 @@
 # read -s -p "Enter sudo password: " passy
 # printf "%s" $passy |sudo -u ${username} -i ... 
 
-username=bwilson
-
 #create_default_dirs () { 
  
 #  if [[ ! -z $(which xdg-user-dirs-update) ]]
@@ -19,14 +17,16 @@ username=bwilson
 #  fi
 #} 
 
+username='pfan'
+
 create_default_dirs () { 
  
-  if [[ ! -z $(which xdg-user-dirs-update) ]]
+  if [[ -n $(command -v xdg-user-dirs-update) ]]
   then
   #su - ${username} -c "xdg-user-dirs-update"
   ### if you want to use sudo instead of su:
   read -s -p "Enter sudo password: " passy
-  printf "%s" $passy |sudo -u {username} -i xdg-user-dirs-update
+  printf "%s" "$passy" |sudo -u $username -i xdg-user-dirs-update
   #sudo -u ${username} -i xdg-user-dirs-update
   fi
 } 
