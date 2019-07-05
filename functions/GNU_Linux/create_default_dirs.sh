9302b44830f5327c0372e1bf52861b0f2385411b
marshki <marshall@morethantomorrow.org>
2019-07-05 10:48:10 -0400

    fix comments

--- functions/GNU_Linux/create_default_dirs.sh
+++ functions/GNU_Linux/create_default_dirs.sh
@@ -10,9 +10,8 @@ create_default_dirs () {
   then
   su ${username} -c xdg-user-dirs-update  
   ### if you want to use sudo instead of su:
-  ### read -s -p "Enter sudo password: " passy
-  ### printf "%s" "$passy" |sudo -u $username -i xdg-user-dirs-update
-  ### sudo -u ${username} -i xdg-user-dirs-update
+  # read -s -p "Enter sudo password: " passy
+  # printf "%s" "$passy" |sudo -u $username -i xdg-user-dirs-update
   fi
 } 
 
