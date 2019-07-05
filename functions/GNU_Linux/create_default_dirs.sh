85e7be1dacdff7938d20f80004ff1cb866941bd6
macdaddy <macdaddy@macdaddys-MBP.fios-router.home>
2019-07-04 22:45:39 -0400

    for testing

--- functions/GNU_Linux/create_default_dirs.sh
+++ functions/GNU_Linux/create_default_dirs.sh
@@ -9,12 +9,24 @@
 
 username=bwilson
 
+#create_default_dirs () { 
+ 
+#  if [[ ! -z $(which xdg-user-dirs-update) ]]
+#  then
+  #su - ${username} -c "xdg-user-dirs-update"
+  ### if you want to use sudo instead of su:
+  #sudo -u ${username} -i xdg-user-dirs-update
+#  fi
+#} 
+
 create_default_dirs () { 
  
   if [[ ! -z $(which xdg-user-dirs-update) ]]
   then
   #su - ${username} -c "xdg-user-dirs-update"
   ### if you want to use sudo instead of su:
+  read -s -p "Enter sudo password: " passy
+  printf "%s" $passy |sudo -u {username} -i xdg-user-dirs-update
   #sudo -u ${username} -i xdg-user-dirs-update
   fi
 } 
