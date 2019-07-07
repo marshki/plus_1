ea300b4f2b134ccdd60b60cf8a888df2b44963e7
marshki <marshall@morethantomorrow.org>
2019-07-07 09:02:11 -0400

    add alt functions

--- functions/GNU_Linux/get_input.sh
+++ functions/GNU_Linux/get_input.sh
@@ -50,9 +50,16 @@ get_input(){
 
 get_input
 
-#read -rp "Enter user name to add and press [Enter]: " username
-#printf "%s\n" "$username"
-#read -rp "Enter 'real' name to add and press [Enter]: " realname
-#printf "%s\n" "$realname"
-#read -rp "Enter password to add and press [Enter]: " pass1
-#printf "%s\n" "$pass1"
+get_username() { 
+  read -rp "Enter user name to add and press [Enter]: " username
+  printf "%s\n" "$username"
+
+get_realname() { 
+  read -rp "Enter 'real' name to add and press [Enter]: " realname
+  printf "%s\n" "$realname"
+} 
+
+get_pass() { 
+  read -rp "Enter password to add and press [Enter]: " pass1
+  printf "%s\n" "$pass1"
+} 
