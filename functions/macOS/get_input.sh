ffbea810a3c644a7bef421bf752b428e0d893281
marshki <marshall@morethantomorrow.org>
2019-07-08 09:51:59 -0400

    remove array for shell check readability

--- functions/macOS/get_input.sh
+++ functions/macOS/get_input.sh
@@ -1,19 +1,6 @@
 #!/bin/bash 
 # Get user input   
 
-# Associative array for variable assignments. 
-
-ASSIGN=(
-username
-realname
-primarygroup
-passhint
-pass1
-)
-
-#!/bin/bash
-# Get user input
-
 # read -r (esc. key binding) & present prompt
 # assign user input to variable
 
@@ -28,31 +15,29 @@ get_realname() {
 }
 
 get_primarygroup() { 
-  read -rp "Enter primary group to add and press [Enter]: " "${ASSIGN[2]}"
+  printf "%s\n" "Primary Group ID: 80=admin, 20=standard" 
+  read -rp "Enter primary group to add and press [Enter]: " primarygroup
 
   printf "%s\n" "$primarygroup"
+} 
+
+get_hint() { 
+  read -rp "Enter hint to add and press [Enter]: " passhint
+
+  printf "%s\n" "$passhint"
 } 
 
 get_pass() {
   read -rp "Enter password to add and press [Enter]: " pass1
   printf "%s\n" "$pass1"
 }
+
 get_input(){ 
   get_username
   get_realname
   get_primarygroup
   get_hint
   get_pass
-} 
-
-get_input
-
-
-
-get_hint() { 
-  read -rp "Enter ${PROMPT[3]} to add and press [Enter]: " "${ASSIGN[3]}"
-
-  printf "%s\n" "$passhint"
 } 
 
 get_input
