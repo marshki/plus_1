bc6cae4aabf5b18edf57330eceb2044df551dd5f
marshki <marshall@morethantomorrow.org>
2019-07-08 09:07:17 -0400

    remove array; not needed here

--- functions/GNU_Linux/get_input.sh
+++ functions/GNU_Linux/get_input.sh
@@ -1,58 +1,13 @@
 #!/bin/bash 
 # Get user input   
 
-# Associative array for user prompts.  
-# Strictly speaking, the array is not needed.
-# It's here as a test for the larger program.
-
-
-PROMPT=(
-"user name"
-"'real' name"
-"password"
-)
-
-# Associative array for variable assignments. 
-
-ASSIGN=(
-username
-realname
-pass1
-)
-
 # read -r (esc. key binding) & present prompt  
 # assign user input to variable 
 
 get_username() { 
-  read -rp "Enter ${PROMPT[0]} to add and press [Enter]: " "${ASSIGN[0]}" 
-
-  printf "%s\n" "$username"
-} 
-
-get_realname() { 
-  read -rp "Enter ${PROMPT[1]} to add and press [Enter]: " "${ASSIGN[1]}"
-
-  printf "%s\n" "$realname"
-} 
-
-get_pass() {
-  read -rp "Enter ${PROMPT[2]} to add and press [Enter]: " "${ASSIGN[2]}"
-
-  printf "%s\n" "$pass1"
-
-} 
-
-get_input(){ 
-  get_username
-  get_realname
-  get_pass
-} 
-
-get_input
-
-get_username() { 
   read -rp "Enter user name to add and press [Enter]: " username
   printf "%s\n" "$username"
+} 
 
 get_realname() { 
   read -rp "Enter 'real' name to add and press [Enter]: " realname
@@ -63,3 +18,12 @@ get_pass() {
   read -rp "Enter password to add and press [Enter]: " pass1
   printf "%s\n" "$pass1"
 } 
+
+get_input() { 
+  get_username
+  get_realname
+  get_pass
+} 
+
+get_input
+ 
