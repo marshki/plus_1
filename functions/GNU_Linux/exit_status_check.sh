f929edb488eb29f1e340243d9cdec61456d87ac9
marshki <marshall@morethantomorrow.org>
2019-07-17 20:55:57 -0400

    add comments

--- functions/GNU_Linux/exit_status_check.sh
+++ functions/GNU_Linux/exit_status_check.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Check exit status of program. 
+# The print statments are mostly for demonstrative purposes. 
 
 print_me () { 
   printf "%s\\n" "Print me."  
@@ -11,7 +12,7 @@ retVal=$?
 
 exit_status () { 
   if [[ $retVal -ne 0 ]]; then
-    printf "%s\\n" "Something went wrong, homie..."
+    printf "%s\\n" "Something went wrong, homie."
   else
     printf "%s\\n" "Done."
   fi
