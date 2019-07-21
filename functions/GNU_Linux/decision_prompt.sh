54150f4e4d43b5f3b08b0e9b02352adf80bcaada
marshki <marshall@morethantomorrow.org>
2019-07-21 10:25:30 -0400

    for testing purposes

--- functions/GNU_Linux/decision_prompt.sh
+++ functions/GNU_Linux/decision_prompt.sh
@@ -1,11 +1,33 @@
 #!/bin/bash 
 # Should script add default directory structure? Yes/No? 
 
-read -r -p "Add default directory structure (y/n)? " PROMPT
+#read -r -p "Add default directory structure (y/n)? " PROMPT
 
-if [ "$PROMPT" = "y" ]; then 
-  printf "%s\\n" "Add 'em."; 
-else
-  printf "%s\\n" "Do not add 'em."; 
-fi
+#if [ "$PROMPT" = "y" ]; then 
+#  printf "%s\\n" "Add 'em."; 
+#else
+#  printf "%s\\n" "Do not add 'em."; 
+#fi
+
+
+#create_default_dirs () { 
+#  printf "%s\\n" "Creating default directories..." 
  
+#  if [[ -n $(command -v xdg-user-dirs-update) ]]
+#  then
+#  su "${username}" -c xdg-user-dirs-update  
+#  fi
+#}  
+
+create_default_dirs () { 
+  read -r -p "Add default directory structure (y/n)? " PROMPT
+
+  if [[ "$PROMPT" = "y" ]] && [[ -n $(command -v xdg-user-dirs-update) ]]
+  then 
+    printf "%s\\n" "Creating default directories..." 
+
+    su "${username}" -c xdg-user-dirs-update 
+  else
+    continue
+  fi
+}   
