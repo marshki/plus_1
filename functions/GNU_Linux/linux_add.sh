5ea09f22880d66975a4315bf195155b50f6b46ab
marshki <marshall@morethantomorrow.org>
2019-07-27 10:55:38 -0400

    rm excess log references

--- functions/GNU_Linux/linux_add.sh
+++ functions/GNU_Linux/linux_add.sh
@@ -2,14 +2,6 @@
 # Create local user account in GNU/Linux 
 # via `user add` utility. 
 
-LOG_FILE="plus_1.sh.log"
-
-# Log function. 
-
-log () {
-  printf "%s\n" "$(date +"%b %d %X :") $*" |tee -a "$LOG_FILE"
-}
-
 # Is current UID 0? If not, exit. (Not needed for non-admin account creation).
 
 root_check () {
@@ -80,7 +72,7 @@ user_info() {
 create_user() { 
   printf "%s\\n" "Adding user..." 
 
-  log "$(useradd --create-home --user-group --home /home/"$username" --comment "$realname" --shell /bin/bash "$username")" 
+  useradd --create-home --user-group --home /home/"$username" --comment "$realname" --shell /bin/bash "$username" 
 } 
 
 # Set password. 
