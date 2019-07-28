1a0cc530ac6e4b89b64b3b318353df50dd6905fe
marshki <marshall@morethantomorrow.org>
2019-07-28 08:38:43 -0400

    update

--- functions/cross_platform/user_check_loop.sh
+++ functions/cross_platform/user_check_loop.sh
@@ -4,20 +4,18 @@
 # send ID and standard error to /dev/null
 
 #name='lilmac'
-
-
-
 while true
 
-read -r -p "Enter username: " name
+read -r -p "Enter username to add and press [Enter]: " username 
 
-do
-   if id "$name" >/dev/null 2>&1;then
-      printf "%s\\n" "ERROR: $name already exists. Try again."
-   else 
-      printf "%s\\n" "$name does not exist. Continuing."   
-      break
-   fi
+do 
+  if id "$username" >/dev/null 2>&1;then
+    printf "%s\\n" "ERROR: $username already exists. Try again."
+  else 
+    printf "%s\\n" "$username does not exist. Continuing."   
+    break
+  fi
+  
 done
 
 #username_check() {
