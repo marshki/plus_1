6767bbfa1d19abbe94f13515d5eeac5ca185c31c
marshki <marshall@morethantomorrow.org>
2019-07-29 09:25:15 -0400

    add printf for spacing

--- functions/cross_platform/get_password_loop.sh
+++ functions/cross_platform/get_password_loop.sh
@@ -1,25 +1,24 @@
 #!/bin/bash 
-# Password prompt (twice). 
-# Exit if passwords do not match.
-
-# printf "%s\n" "$pass1" and "$pass2" for illustrative purposes, 
-# you almost certainly don't want this
+# Prompt user for input (twice) until both inputs match. 
 
 # read -r (esc. key binding), -s silently, & -p present prompt  
 
-while true 
-do
-  read -r -s -p "Enter password to add and press [Enter]: " pass1 
-  read -r -s -p "Re-enter password to add and press [Enter]: " pass2 
+get_password () { 
 
-  if [[ "$pass1" != "$pass2" ]]; then 
-    printf "%s\n" "ERROR: Passwords do no match."
-  else 
-    printf "%s\n" "Passwords match. Continuing..."
-    break
-  fi 
-done
+  while true 
+  do
+    read -r -s -p "Enter password to add and press [Enter]: " pass1 
+    printf "\\n" 
+    read -r -s -p "Re-enter password to add and press [Enter]: " pass2 
+    printf "\\n" 
 
-#get_password
-#confirm_password
-#check_password
+    if [[ "$pass1" != "$pass2" ]]; then 
+      printf "%s\n" "ERROR: Passwords do no match."
+    else 
+      printf "%s\n" "Passwords match. Continuing..."
+      break
+    fi 
+  done
+} 
+
+get_password
