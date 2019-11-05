0c2c2fc0190cf425efa1b32598352732ce97d197
marshki <marshall@morethantomorrow.org>
2019-11-05 10:47:48 -0500

    test for sudo or wheel group

--- functions/GNU_Linux/add_admin_user.sh
+++ functions/GNU_Linux/add_admin_user.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-# Check OS for administrative group via `getent`, 
-# then, using `usermod`, add user to group. 
-# sudo = Debian-based 
-# wheel = RHEL-based 
+# Check OS for administrative group via `getent`,
+# then, using `usermod`, add user to group.
+# sudo = Debian-based
+# wheel = RHEL-based
 
-admin_group_check () { 
-    if ! getent group sudo || ! getent group wheel
-    then 
+admin_group_check () {
+    if ! [ "$(getent group sudo)" ] && ! [ "$(getent group wheel)" ]
+    then
         printf "%s\\n" "No admin group found."
     fi
 }
-admin_group_check 
+
+admin_group_check
