326142314b85c9ef5895fa39e07c54e784547cd6
marshki <marshall@morethantomorrow.org>
2019-11-07 11:44:12 -0500

    Revert "update"
    This reverts commit 491754d58670679f2fa39c32523b34da4a33cbc5.

--- src/plus_1.sh
+++ src/plus_1.sh
@@ -100,44 +100,12 @@ create_default_dirs () {
   fi
 }
 
-# Add user to admin group. 
-
-add_admin_user () {
-
-  read -r -p "Add user to administrator (sudo/wheel) group [yes/no]? " PROMPT
-
-  if [[ "$PROMPT" = "yes" ]]
-  then 
-    printf "%s\\n" "Checking for administrator group..."
-    
-    if [ "$(getent group sudo)" ]
-    then
-        printf "%s\\n" "Adding user to sudo group..."
-        usermod --append --groups sudo "$username"
-
-    elif [ "$(getent group wheel)" ]
-    then 
-        printf "%s\\n" "Adding user to wheel group..."
-        usermod --append --groups wheel "$username"
-
-    else 
-        if ! [ "$(getent group sudo)" ] && ! [ "$(getent group wheel)" ]
-        then
-            printf "%s\\n" "ERROR: No admin group found. Exiting." >&2
-            exit 1
-        fi
-    fi
-  fi
-}
-
-
 # GNU/Linux wrapper.
 
 add_linux() {
   create_user_linux
   set_password_linux
   create_default_dirs
-  add_admin_user
 } 
 
 #=======================================
