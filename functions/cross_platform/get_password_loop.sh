12f5a74e9607807fe2c652774b243661e587fa8e
marshki <marshall@morethantomorrow.org>
2021-09-15 09:32:45 -0400

    shebang line, comments, and rm / in printf

--- functions/cross_platform/get_password_loop.sh
+++ functions/cross_platform/get_password_loop.sh
@@ -1,18 +1,18 @@
-#!/bin/bash 
-# Prompt user for input (twice) until both inputs match. 
+#!/usr/bin/env bash
+# Prompt user for input (twice) until both inputs match.
 
-# read -r (esc. key binding), -s silently, & -p present prompt  
+# read -r (esc. key binding), -s silently, & -p present prompt
 
-get_password () { 
+get_password () {
 
-  while true 
+  while true
 
   do
     read -r -s -p "Enter password to add and press [Enter]: " pass1 
-    printf "\\n" 
+    printf "\n" 
 
     read -r -s -p "Re-enter password to add and press [Enter]: " pass2 
-    printf "\\n" 
+    printf "\n" 
 
     if [[ "$pass1" != "$pass2" ]]; then 
       printf "%s\n" "ERROR: Passwords do no match."
