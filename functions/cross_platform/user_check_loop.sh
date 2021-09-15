c48b534ba850404aabf97cd81dbc2592d1671968
marshki <marshall@morethantomorrow.org>
2021-09-15 10:32:30 -0400

    shebang update, replace // to /

--- functions/cross_platform/user_check_loop.sh
+++ functions/cross_platform/user_check_loop.sh
@@ -1,9 +1,9 @@
-#!/bin/bash 
-# Prompt user for input until non-existent username is entered.        
+#!/usr/bin/emv bash
+# Prompt user for input until non-existent username is entered.
 
 # send ID and standard error to /dev/null
 
-username_check () { 
+username_check () {
 
   while true
 
@@ -11,9 +11,9 @@ username_check () {
     read -r -p "Enter username to add and press [Enter]: " username  
 
     if id "$username" >/dev/null 2>&1; then
-      printf "%s\\n" "ERROR: $username already exists. Try again."
+      printf "%s\n" "ERROR: $username already exists. Try again."
     else 
-      printf "%s\\n" "$username does not exist. Continuing..."   
+      printf "%s\n" "$username does not exist. Continuing..."   
       break
     fi
   
