4f3608b23d75b88098bce7f694fb19e2d08de372
marshki <marshall@morethantomorrow.org>
2021-09-15 10:37:20 -0400

    update shebang

--- functions/GNU_Linux/create_user.sh
+++ functions/GNU_Linux/create_user.sh
@@ -1,13 +1,13 @@
-#!/bin/bash 
-# Add user via useradd utility. 
+#!/usr/bin/env bash
+# Add user via useradd utility.
 
 username='sjobs'
 realname='Steve Jobs'
 
-create_user() { 
-  printf "%s\\n" "Adding user..." 
+create_user() {
+  printf "%s\\n" "Adding user..."
 
-  useradd --create-home --user-group --home /home/"$username" --comment "$realname" --shell /bin/bash "$username" 
-} 
+  useradd --create-home --user-group --home /home/"$username" --comment "$realname" --shell /bin/bash "$username"
+}
 
 create_user
