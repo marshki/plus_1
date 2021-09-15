c8e62e6fa10c2c7fff51dea4b4c908247aaafc03
marshki <marshall@morethantomorrow.org>
2021-09-15 10:34:38 -0400

    cosmetic update

--- functions/GNU_Linux/add_admin_user.sh
+++ functions/GNU_Linux/add_admin_user.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/use/bin/env bash
 # Check OS for administrative group via `getent`,
 # then, using `usermod`, add user to group.
 # sudo = Debian-based
@@ -12,22 +12,22 @@ add_admin_user () {
 
   if [[ "$PROMPT" = "yes" ]]
   then 
-    printf "%s\\n" "Checking for administrator group."
+    printf "%s\n" "Checking for administrator group."
     
     if [ "$(getent group sudo)" ]
     then
-        printf "%s\\n" "Adding user to sudo group..."
+        printf "%s\n" "Adding user to sudo group..."
         usermod --append --groups sudo "$username"
 
     elif [ "$(getent group wheel)" ]
     then 
-        printf "%s\\n" "Adding user to wheel group..."
+        printf "%s\n" "Adding user to wheel group..."
         usermod --append --groups wheel "$username"
 
-    else 
+    else
         if ! [ "$(getent group sudo)" ] && ! [ "$(getent group wheel)" ]
         then
-            printf "%s\\n" "ERROR: No admin group found. Exiting." >&2
+            printf "%s\n" "ERROR: No admin group found. Exiting." >&2
             exit 1
         fi
     fi
