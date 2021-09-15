0f7ad961e2187ba027f4efa282dc9a0a966bea21
marshki <marshall@morethantomorrow.org>
2021-09-15 10:59:18 -0400

    update shebang, spacing

--- functions/macOS/get_uid.sh
+++ functions/macOS/get_uid.sh
@@ -1,17 +1,17 @@
-#!/bin/bash 
-# Get the highest UID and increment +1 
+#!/usr/bin/env bash
+# Get the highest UID and increment +1.
 
-# sort numerically by second field 
+# sort numerically by second field
 # pipe to awk to print last record from second field
 # increment +1
 
-get_uid (){ 
+get_uid (){
 
   uid=$(dscl . -list /Users UniqueID |sort --numeric-sort --key=2 |awk 'END{print $2}')
-  printf "%s\\n" "Highest UID: $uid"
+  printf "%s\n" "Highest UID: $uid"
 
   increment_uid=$((uid +1))
-  printf "%s\\n" "Next UID: $increment_uid"
-} 
+  printf "%s\n" "Next UID: $increment_uid"
+}
 
 get_uid
