f5b236ef686b2fd14f8bae8b9cb965f0ae9b2785
marshki <marshall@morethantomorrow.org>
2021-09-15 10:39:47 -0400

    update shebang, spacing

--- functions/GNU_Linux/decision_prompt.sh
+++ functions/GNU_Linux/decision_prompt.sh
@@ -1,31 +1,31 @@
-#!/bin/bash 
-# Should script add default directory structure? Yes/No? 
+#!/usr/bin/env bash 
+# Should script add default directory structure? Yes/No?
 
-# This is a test prompt. 
+# This is a test prompt.
 
-decision_prompt () { 
+decision_prompt () {
   read -r -p "Add default directory structure (y/n)? " PROMPT
 
-  if [[ "$PROMPT" = "y" ]]; then 
-    printf "%s\\n" "Add 'em."; 
-  else 
-    printf "%s\\n" "Do not add 'em."; 
+  if [[ "$PROMPT" = "y" ]]; then
+    printf "%s\\n" "Add 'em.";
+  else
+    printf "%s\\n" "Do not add 'em.";
   fi
-} 
+}
 
-# This is the actual prompt. 
+# This is the actual prompt.
 
 username='sjobs'
 
-create_default_dirs () { 
+create_default_dirs () {
   read -r -p "Add default directory structure (y/n)? " PROMPT
 
   if [[ "$PROMPT" = "y" ]] && [[ -n $(command -v xdg-user-dirs-update) ]]
-  then 
-    printf "%s\\n" "Creating default directories..." 
+  then
+    printf "%s\\n" "Creating default directories..."
 
-    su "${username}" -c xdg-user-dirs-update 
+    su "${username}" -c xdg-user-dirs-update
   fi
-} 
+}
 
-create_default_dirs  
+create_default_dirs
