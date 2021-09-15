942a3f0a435fccbb9e6dbd42e364f33f2264fc68
marshki <marshall@morethantomorrow.org>
2021-09-15 10:53:49 -0400

    fix spacing, update shebang

--- functions/macOS/create_user.sh
+++ functions/macOS/create_user.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Add user via dscl utility.
-# Adds user WITHOUT creating directory in /Users
-
+# Adds user WITHOUT creating directory in /Users.
 
 username='sjobs'
 increment_uid='502'
@@ -10,17 +9,17 @@ primarygroup='20'
 passhint='1...'
 pass2='1morething'
 
-create_user() { 
-  printf "%s\\n" "Adding user..." 
+create_user() {
+  printf "%s\\n" "Adding user..."
 
   dscl . -create /Users/"$username"
-  dscl . -create /Users/"$username" UniqueID "$increment_uid" 
+  dscl . -create /Users/"$username" UniqueID "$increment_uid"
   dscl . -create /Users/"$username" UserShell /bin/bash
   dscl . -create /Users/"$username" RealName "$realname"
   dscl . -create /Users/"$username" PrimaryGroupID "$primarygroup"
   dscl . -create /Users/"$username" NFSHomeDirectory /Users/$username
   dscl . -create /Users/"$username" hint "$passhint"
   dscl . -passwd /Users/"$username" "$pass2"
-} 
+}
 
 create_user
