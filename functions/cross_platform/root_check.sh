f1d1660da79de79edcc84208dfddcae13a51ebc2
marshki <marshall@morethantomorrow.org>
2021-09-15 10:20:30 -0400

    comaprison operator for number should be !=, also rm / in printf, update comments

--- functions/cross_platform/root_check.sh
+++ functions/cross_platform/root_check.sh
@@ -1,14 +1,14 @@
-#!/bin/bash 
+#!/usr/bin/env bash
 # Is current UID 0? If not, exit.
 
-# Root UID is 0 
-# if user's UID is not 0 redirect output to standard error, then exit.   
+# Root UID is 0.
+# If user's UID is not 0 redirect output to standard error, then exit.
 
 root_check () {
-  if [ "$EUID" -ne "0" ] ; then
-    printf "%s\\n" "ERROR: Root privileges are required to continue. Exiting." >&2
+  if [ "$EUID" != "0" ] ; then
+    printf "%s\n" "ERROR: Root privileges are required to continue. Exiting." >&2
     exit 1
 fi
-} 
+}
 
-root_check 
+root_check
