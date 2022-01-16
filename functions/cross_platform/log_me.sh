9851e0bf4824114dc64c75b39af870cd299212f2
marshki <marshall@morethantomorrow.org>
2022-01-16 13:21:53 -0500

    add hashtag; rm comment

--- functions/cross_platform/log_me.sh
+++ functions/cross_platform/log_me.sh
@@ -11,13 +11,11 @@ log "Now hear dis!"
 
 # Use like this, e.g.: 
 
-# Replace 1st occurrence of "find_string" with "replace_string" in all files.
-
 #string_rename() {
 #  printf "%s\n\n" "Renaming files..."
 
 #  for file in *; do
-    log "$(mv -v "$file" "${file/$find_string/$replace_string}")"
+#    log "$(mv -v "$file" "${file/$find_string/$replace_string}")"
 #  done
 
 #  printf "\n%s\n\n" "Done."
