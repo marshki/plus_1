4ebf7c248d00de7166a81f8995db7883d6e28007
marshki <marshall@morethantomorrow.org>
2022-01-16 13:10:33 -0500

    add log function as placeholder

---
+++ functions/cross_platform/log_me.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash 
+# Create log file and write to it when called. 
+
+LOG_FILE="bulky.sh.log"
+
+log () {
+  printf "%s\n" "$(date +"%b %d %X :") $*" |tee -a "$LOG_FILE"
+}
+
+log "Now hear dis!"
+
+# Use like this, e.g.: 
+
+# Replace 1st occurrence of "find_string" with "replace_string" in all files.
+
+#string_rename() {
+#  printf "%s\n\n" "Renaming files..."
+
+#  for file in *; do
+    log "$(mv -v "$file" "${file/$find_string/$replace_string}")"
+#  done
+
+#  printf "\n%s\n\n" "Done."
+#}
