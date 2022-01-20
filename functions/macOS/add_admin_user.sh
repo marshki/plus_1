5981031c025a66d5f696dba6c2e64f18cdd5beca
marshki <marshall@morethantomorrow.org>
2022-01-20 14:50:04 -0500

    update comments; stick with dseditgroup command

--- functions/macOS/add_admin_user.sh
+++ functions/macOS/add_admin_user.sh
@@ -1,13 +1,7 @@
 #!/use/bin/env bash
 
-# Option 1: 
-# Check OS for administrative group via `dscl`: 
-# dscl . read /Groups/admin
-
-# Option 2 (probably preferable, but need to test): 
 # Check OS for administrative group via `dsedit`:
 # dseditgroup -o read admin
-
 # then, using `dseditgroup `, add user to group:
 # dseditgroup -o edit -a username -t user admin
 
@@ -20,14 +14,14 @@ add_admin_user () {
   if [[ "$PROMPT" = "yes" ]]
   then 
     printf "%s\n" "Checking for admin group."
-    
-    if [ "$(dscl . read /Groups/poop)" ]
+
+    if [ "$(dseditgroup -o read poop)" ]
     then
         printf "%s\n" "Adding user to admin group..."
         #dseditgroup -o edit -a username -t "$username" admin
 
     else
-        if ! [ "$(dscl . read /Groups/poop)" ]
+        if ! [ "$(dseditgroup -o read poop)" ]        
         then
             printf "%s\n" "ERROR: No admin group found. Exiting." >&2
             exit 1
