2af5254696dc458a2ade7702a0d62184cc724481
marshki <marshall@morethantomorrow.org>
2022-01-21 13:16:26 -0500

    add

---
+++ functions/macOS/add_wheel_user.sh
@@ -0,0 +1,34 @@
+#!/use/bin/env bash
+# This is works, but is here for reference, 
+# and not included in the larger script.
+
+# Check OS for administrative group via `dseditgroup`,
+# then, using `dseditgroup `, add user to group.
+# admin = macOS
+
+username='sjobs'
+
+add_admin_user () {
+
+  read -r -p "Add user to admin group [yes/no]? " PROMPT
+
+  if [[ "$PROMPT" = "yes" ]]
+  then 
+    printf "%s\n" "Checking for admin group."
+
+    if [ "$(dseditgroup -o read admin 2>/dev/null)" ]
+    then
+        printf "%s\n" "Adding user to admin group..."
+        #dseditgroup -o edit -a username -t "$username" admin
+
+    else
+        if ! [ "$(dseditgroup -o read admin 2>/dev/null)" ]
+        then
+            printf "%s\n" "ERROR: No admin group found. Exiting." >&2
+            exit 1
+        fi
+    fi
+  fi
+}
+
+add_admin_user
