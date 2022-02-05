fb9d8dbeae95464ca10f1bed9abfee46acc7b55d
marshki <marshall@morethantomorrow.org>
2022-02-05 14:56:50 +0000

    fix ShellCheck formatting error

--- src/GNU_Linux/linux_add.sh
+++ src/GNU_Linux/linux_add.sh
@@ -84,7 +84,7 @@ create_user() {
   printf "%s\n" "Adding user..."
 
   $(useradd --create-home --user-group --home /home/"$username" --comment "$realname" --shell /bin/bash "$username")
-  log "new user: name="$username", home=/home/"$username", shell=/bin/bash"
+  log "new user: name='$username', home=/home/'$username', shell=/bin/bash"
 
 }
 
