511712bbe9f4e674f93d5ed1b8194615c2ea4ea4
marshki <marshall@morethantomorrow.org>
2023-09-24 12:09:51 -0400

    refactor

--- functions/GNU_Linux/add_admin_user.sh
+++ functions/GNU_Linux/add_admin_user.sh
@@ -6,30 +6,30 @@
 
 username='sjobs'
 
-add_admin_user () {
+add_admin_user() {
 
   read -r -p "Add user to administrator (sudo/wheel) group [yes/no]? " PROMPT
 
-  if [[ "$PROMPT" = "yes" ]]
-  then 
+  if [[ "$PROMPT" = "yes" ]]; then
+
     printf "%s\n" "Checking for administrator group."
     
-    if [ "$(getent group sudo)" ]
-    then
-        printf "%s\n" "Adding user to sudo group..."
-        usermod --append --groups sudo "$username"
+    if [ "$(getent group sudo)" ]; then
+	    
+      printf "%s\n" "Adding user to sudo group..."
+      usermod --append --groups sudo "$username"
 
-    elif [ "$(getent group wheel)" ]
-    then 
-        printf "%s\n" "Adding user to wheel group..."
-        usermod --append --groups wheel "$username"
+    elif [ "$(getent group wheel)" ]; then
+
+      printf "%s\n" "Adding user to wheel group..."
+      usermod --append --groups wheel "$username"
 
     else
-        if ! [ "$(getent group sudo)" ] && ! [ "$(getent group wheel)" ]
-        then
-            printf "%s\n" "ERROR: No admin group found. Exiting." >&2
-            exit 1
-        fi
+      if ! [ "$(getent group sudo)" ] && ! [ "$(getent group wheel)" ]; then
+
+        printf "%s\n" "ERROR: No admin group found. Exiting." >&2
+        exit 1
+      fi
     fi
   fi
 }
