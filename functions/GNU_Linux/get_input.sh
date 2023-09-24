af954641a68bc331eb1d9c6fd6c870c229fead50
marshki <marshall@morethantomorrow.org>
2023-09-24 11:44:13 -0400

    improve readability

--- functions/GNU_Linux/get_input.sh
+++ functions/GNU_Linux/get_input.sh
@@ -6,23 +6,26 @@
 
 get_username() {
   read -r -p "Enter user name to add and press [Enter]: " username
+
   printf "%s\n" "$username"
 }
 
 get_realname() {
   read -r -p "Enter 'real' name to add and press [Enter]: " realname
+
   printf "%s\n" "$realname"
 }
 
-get_pass() { 
+get_pass() {
   read -r -p "Enter password to add and press [Enter]: " pass1
+
   printf "%s\n" "$pass1"
 }
 
-get_input() { 
+get_input() {
   get_username
   get_realname
   get_pass
-} 
+}
 
 get_input
