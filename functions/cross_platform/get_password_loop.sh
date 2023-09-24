4b2a0233ad92a6ea277dc4bfdbcb83ee418d7363
marshki <marshall@morethantomorrow.org>
2023-09-24 11:19:15 -0400

    update spacing

--- functions/cross_platform/get_password_loop.sh
+++ functions/cross_platform/get_password_loop.sh
@@ -3,24 +3,28 @@
 
 # read -r (esc. key binding), -s silently, & -p present prompt
 
-get_password () {
+get_password() {
 
   while true
 
   do
-    read -r -s -p "Enter password to add and press [Enter]: " pass1 
-    printf "\n" 
+    read -r -s -p "Enter password to add and press [Enter]: " pass1
 
-    read -r -s -p "Re-enter password to add and press [Enter]: " pass2 
-    printf "\n" 
+    printf "\n"
 
-    if [[ "$pass1" != "$pass2" ]]; then 
+    read -r -s -p "Re-enter password to add and press [Enter]: " pass2
+
+    printf "\n"
+
+    if [[ "$pass1" != "$pass2" ]]; then
+
       printf "%s\n" "ERROR: Passwords do no match."
-    else 
+    else
+
       printf "%s\n" "Passwords match. Continuing..."
       break
-    fi 
+    fi
   done
-} 
+}
 
 get_password
