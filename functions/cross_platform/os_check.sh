8817c955f4278867fe45162e45f52b2d150315ef
marshki <marshall@morethantomorrow.org>
2023-09-24 11:11:16 -0400

    fix spacing

--- functions/cross_platform/os_check.sh
+++ functions/cross_platform/os_check.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 # Detect system architecture, then act.
   
-os_check () {
+os_check() {
 
     case $(uname -s) in
     Darwin)
+
       printf "%s\n" "Darwin"
       ;;
     Linux)
+
       printf "%s\n" "Linux"
       ;;
     *)
+
       printf "%s\n" "He can't handle your speed, $(uname -s)"
       ;;
     esac
