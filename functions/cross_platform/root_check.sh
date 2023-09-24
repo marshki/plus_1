b4790b6855feb34747561a0c079f62c29ce993ef
marshki <marshall@morethantomorrow.org>
2023-09-24 11:00:21 -0400

    fix spacing

--- functions/cross_platform/root_check.sh
+++ functions/cross_platform/root_check.sh
@@ -4,7 +4,7 @@
 # Root UID is 0.
 # If user's UID is not 0 redirect output to standard error, then exit.
 
-root_check () {
+root_check() {
   if [ "$EUID" != "0" ] ; then
     printf "%s\n" "ERROR: Root privileges are required to continue. Exiting." >&2
     exit 1
