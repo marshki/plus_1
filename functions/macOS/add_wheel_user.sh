37d27222ab11c34990aad59dae515bfea4ff267b
marshki <marshall@morethantomorrow.org>
2023-09-26 09:26:33 -0400

    update

--- functions/macOS/add_wheel_user.sh
+++ functions/macOS/add_wheel_user.sh
@@ -6,25 +6,24 @@
 
 username='sjobs'
 
-add_admin_user () {
+add_admin_user() {
 
   read -r -p "Add user to wheel group [yes/no]? " PROMPT
 
-  if [[ "$PROMPT" = "yes" ]]
-  then 
+  if [[ "$PROMPT" = "yes" ]]; then
+
     printf "%s\n" "Checking for wheel group."
 
-    if [ "$(dseditgroup -o read wheel 2>/dev/null)" ]
-    then
-        printf "%s\n" "Adding user to wheel group..."
-        dseditgroup -o edit -a username -t "$username" wheel
+    if [ "$(dseditgroup -o read wheel 2>/dev/null)" ]; then
+
+      printf "%s\n" "Adding user to wheel group..."
+      dseditgroup -o edit -a username -t "$username" wheel
 
     else
-        if ! [ "$(dseditgroup -o read wheel 2>/dev/null)" ]
-        then
-            printf "%s\n" "ERROR: No wheel group found. Exiting." >&2
-            exit 1
-        fi
+      if ! [ "$(dseditgroup -o read wheel 2>/dev/null)" ]; then
+        printf "%s\n" "ERROR: No wheel group found. Exiting." >&2
+          exit 1
+      fi
     fi
   fi
 }
