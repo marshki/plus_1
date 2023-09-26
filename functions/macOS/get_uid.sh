b59f7a40edd6432b80d83a2471d8cc9bc88763e9
marshki <marshall@morethantomorrow.org>
2023-09-26 09:48:38 -0400

    fix spacing

--- functions/macOS/get_uid.sh
+++ functions/macOS/get_uid.sh
@@ -5,12 +5,14 @@
 # pipe to awk to print last record from second field
 # increment +1
 
-get_uid (){
+get_uid() {
 
   uid=$(dscl . -list /Users UniqueID |sort --numeric-sort --key=2 |awk 'END{print $2}')
+
   printf "%s\n" "Highest UID: $uid"
 
   increment_uid=$((uid +1))
+
   printf "%s\n" "Next UID: $increment_uid"
 }
 
