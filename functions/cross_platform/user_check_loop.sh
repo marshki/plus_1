cd6597d1812bd1689dcb940c209c6d00501ed181
marshki <marshall@morethantomorrow.org>
2024-06-20 10:31:46 -0400

    fix comment; while true + do on one line

--- functions/cross_platform/user_check_loop.sh
+++ functions/cross_platform/user_check_loop.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/emv bash
 # Prompt user for input until non-existent username is entered.
 
-# send ID and standard error to /dev/null
+# Send ID and standard error to /dev/null
 
 username_check() {
-  while true
+  while true; do
 
-  do
-    read -r -p "Enter username to add and press [Enter]: " username  
+    read -r -p "Enter username to add and press [Enter]: " username
 
     if id "$username" >/dev/null 2>&1; then
 
     	printf "%s\n" "ERROR: $username already exists. Try again."
-    else 
+    else
 
-      	printf "%s\n" "$username does not exist. Continuing..."   
+      	printf "%s\n" "$username does not exist. Continuing..."
       break
     fi
   
