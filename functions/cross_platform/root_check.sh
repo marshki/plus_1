6388d69adf0bc5f7877e58ac1cea8b540663d518
marshki <marshall@morethantomorrow.org>
2024-06-24 17:43:59 -0400

    replace != with -ne for comparison operator

--- functions/cross_platform/root_check.sh
+++ functions/cross_platform/root_check.sh
@@ -5,7 +5,7 @@
 # If user's UID is not 0 redirect output to standard error, then exit.
 
 root_check() {
-  if [ "$EUID" != "0" ] ; then
+  if [ "$EUID" -ne "0" ] ; then
 
     printf "%s\n" "ERROR: Root privileges are required to continue. Exiting." >&2
     exit 1
