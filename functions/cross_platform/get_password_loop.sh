e0ec17c6a877277a64e3fdc3b1902f52ea832c7b
marshki <marshall@morethantomorrow.org>
2024-06-29 12:48:40 -0400

    refactor for spacing

--- functions/cross_platform/get_password_loop.sh
+++ functions/cross_platform/get_password_loop.sh
@@ -6,20 +6,16 @@
 get_password() {
 
   while true; do
-
     read -r -s -p "Enter password to add and press [Enter]: " pass1
-
     printf "\n"
 
     read -r -s -p "Re-enter password to add and press [Enter]: " pass2
-
     printf "\n"
 
     if [[ "$pass1" != "$pass2" ]]; then
-
       printf "%s\n" "ERROR: Passwords do no match."
-    else
 
+    else
       printf "%s\n" "Passwords match. Continuing..."
       break
     fi
