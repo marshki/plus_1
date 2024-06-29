c4dbc2d3529b92019fe99759e01a8cd27e1b6979
marshki <marshall@morethantomorrow.org>
2024-06-29 12:40:32 -0400

    rm excess spacing

--- functions/GNU_Linux/get_input.sh
+++ functions/GNU_Linux/get_input.sh
@@ -6,19 +6,16 @@
 
 get_username() {
   read -r -p "Enter user name to add and press [Enter]: " username
-
   printf "%s\n" "$username"
 }
 
 get_realname() {
   read -r -p "Enter 'real' name to add and press [Enter]: " realname
-
   printf "%s\n" "$realname"
 }
 
 get_pass() {
   read -r -p "Enter password to add and press [Enter]: " pass1
-
   printf "%s\n" "$pass1"
 }
 
