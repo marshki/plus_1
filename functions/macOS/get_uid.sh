195339542fb48716bc646f31c4cc6084eb46beb3
marshki <marshall@morethantomorrow.org>
2024-07-01 17:53:41 -0400

    refacotr for readability

--- functions/macOS/get_uid.sh
+++ functions/macOS/get_uid.sh
@@ -6,13 +6,10 @@
 # increment +1
 
 get_uid() {
-
   uid=$(dscl . -list /Users UniqueID |sort --numeric-sort --key=2 |awk 'END{print $2}')
-
   printf "%s\n" "Highest UID: $uid"
 
   increment_uid=$((uid +1))
-
   printf "%s\n" "Next UID: $increment_uid"
 }
 
