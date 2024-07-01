3b32e1797592a57311e555fb59e7f4244ffbf68e
marshki <marshall@morethantomorrow.org>
2024-07-01 17:52:52 -0400

    update spacing

--- functions/macOS/get_input.sh
+++ functions/macOS/get_input.sh
@@ -5,34 +5,29 @@
 # assign user input to variable.
 
 get_username() {
-
   read -rp "Enter user name to add and press [Enter]: " username
   printf "%s\n" "$username"
 }
 
 get_realname() {
-
   read -rp "Enter 'real' name to add and press [Enter]: " realname
   printf "%s\n" "$realname"
 }
 
 get_primarygroup() { 
-
   printf "%s\n" "Primary Group ID: 80=admin, 20=standard"
-  read -rp "Enter primary group to add and press [Enter]: " primarygroup
 
+  read -rp "Enter primary group to add and press [Enter]: " primarygroup
   printf "%s\n" "$primarygroup"
 }
 
 get_hint() {
   read -rp "Enter hint to add and press [Enter]: " passhint
-
   printf "%s\n" "$passhint"
 }
 
 get_pass() {
   read -rp "Enter password to add and press [Enter]: " pass1
-
   printf "%s\n" "$pass1"
 }
 
