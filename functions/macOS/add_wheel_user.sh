0c15352b71f5b231153b0c16c0a9e81ff984c0c5
marshki <marshall@morethantomorrow.org>
2024-07-02 10:38:14 -0400

    fix spacing

--- functions/macOS/add_wheel_user.sh
+++ functions/macOS/add_wheel_user.sh
@@ -11,11 +11,9 @@ add_admin_user() {
   read -r -p "Add user to wheel group [yes/no]? " PROMPT
 
   if [[ "$PROMPT" = "yes" ]]; then
-
     printf "%s\n" "Checking for wheel group."
 
     if [ "$(dseditgroup -o read wheel 2>/dev/null)" ]; then
-
       printf "%s\n" "Adding user to wheel group..."
       dseditgroup -o edit -a username -t "$username" wheel
 
