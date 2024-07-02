357de3ee117f70586a538f59cca608eeb0eb8bd3
marshki <marshall@morethantomorrow.org>
2024-07-02 15:37:28 -0400

    add

--- functions/macOS/create_user_too.sh
+++ functions/macOS/create_user_too.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
 # sysadminctl implementation of create_user.sh in this directory
 
-sysadminctl -addUser username -fullName "User Name" -UID 1001 -shell /bin/bash -password password
+# For example (test this!): sysadminctl -addUser username -fullName "User Name" -UID 1001 -shell /bin/bash -password password
+
+create_user() {
+  printf "%s\n" "Adding user..."
+
+  sysadminctl -addUser "$username" \
+              -fullName "$realname" \
+              -UID "$increment_uid" \
+              -shell /bin/bash \
+              -password "$pass2" \
+              -home /Users/"$username"
+
+  log "New user created: name='$username', home=/Users/'$username', shell=/bin/bash"
+}
+
