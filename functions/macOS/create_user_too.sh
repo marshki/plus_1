2da699d76dceee8ee534d26725b7d45f67592012
marshki <marshall@morethantomorrow.org>
2024-07-02 14:15:12 -0400

    add comments

--- functions/macOS/create_user_too.sh
+++ functions/macOS/create_user_too.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+# sysadminctl implementation of create_user.sh in this directory
 
 sysadminctl -addUser username -fullName "User Name" -UID 1001 -shell /bin/bash -password password
