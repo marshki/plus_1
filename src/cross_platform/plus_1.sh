07f6c65618d2aff0ec719f6690effbe884ebbcbd
marshki <marshall@morethantomorrow.org>
2024-07-12 17:21:33 -0400

    refactor main function for spacing, readability

--- src/cross_platform/plus_1.sh
+++ src/cross_platform/plus_1.sh
@@ -250,24 +250,18 @@ plus_1() {
 }
 
 main() {
-
   root_check
-
   printf "%s\n" "plus_1: A Bash script to create local user accounts in GNU/Linux & macOS."
 
   while true; do
-  
     read -r -p "Create user account? (yes/no): " answer
 
     if [ "$answer" = yes ]; then
-
       printf "%s\n" "Let's add a user..."
-
       user_info
       plus_1
 
     else
-
       printf "%s\n" "Exiting."
       exit 0
     fi
