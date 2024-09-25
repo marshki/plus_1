5de49bdecb6d214987ac31ad6dc121268967217b
marshki <marshall@morethantomorrow.org>
2024-09-25 11:05:40 -0400

    comments

--- functions/cross_platform/root_check.sh
+++ functions/cross_platform/root_check.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-# Is current UID 0? If not, exit.
-# Root UID is 0.
-# If user's UID is not 0 redirect output to standard error, then exit.
+#
+# Is current UID 0 (root)? If not, exit.
 
+# If user's UID is not 0 redirect output to standard error, then exit.
 root_check() {
   if [ "$EUID" -ne "0" ] ; then
     printf "%s\n" "ERROR: Root privileges are required to continue. Exiting." >&2
