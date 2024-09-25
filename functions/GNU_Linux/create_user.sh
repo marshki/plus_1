3664447fc794c86d3067b796ed753d970e235463
marshki <marshall@morethantomorrow.org>
2024-09-25 16:48:28 -0400

    add link to man page

--- functions/GNU_Linux/create_user.sh
+++ functions/GNU_Linux/create_user.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+#
 # Add user via useradd utility.
 
 username='sjobs'
 realname='Steve Jobs'
 
+# For reference: https://linux.die.net/man/8/useradd
 create_user() {
   printf "%s\\n" "Adding user..."
   useradd --create-home --user-group --home /home/"$username" --comment "$realname" --shell /bin/bash "$username"
