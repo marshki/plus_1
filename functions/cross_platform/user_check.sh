817b60d6a6883ef3668591c06918b54f1fb2d7ab
marshki <marshall@morethantomorrow.org>
2024-09-25 11:17:05 -0400

    fix comments, spacing

--- functions/cross_platform/user_check.sh
+++ functions/cross_platform/user_check.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
+#
 # Exit if username exists.
-
-# Send ID and standard error to /dev/null
 
 name='someuser'
 
+# Check if id exists. Send id and standard error to /dev/null.
 username_check() {
   if id "$name" >/dev/null 2>&1; then
-
     printf "%s\n" "ERROR: $name already exists. Exiting." >&2
     exit 1
   fi
