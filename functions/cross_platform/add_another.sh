d16172ec7291afc564e4175fe3761d24d2c50047
marshki <marshall@morethantomorrow.org>
2024-09-28 14:45:01 -0400

    fix spacing; add comment

--- functions/cross_platform/add_another.sh
+++ functions/cross_platform/add_another.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
+#
 # While loop prompt to continue/exit
 
 while true; do
-
   read -r -p "Add another user? (yes/no): " answer
-
   if [ "$answer" = yes ]; then
     printf "%s\n" "Adding another user..."
-
   else
     printf "%s\n" "Exiting program..."
     exit 0
   fi
-
 done
