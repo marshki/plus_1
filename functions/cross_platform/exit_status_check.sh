60279f1a58297ab9503a9bf1619471c2043b5739
marshki <marshall@morethantomorrow.org>
2024-09-28 14:42:57 -0400

    update comments

--- functions/cross_platform/exit_status_check.sh
+++ functions/cross_platform/exit_status_check.sh
@@ -3,15 +3,18 @@
 # Check exit status of program.
 # (Print statments are for demonstrative purposes).
 
+# Sample function.
 print_me() { 
   printf "%s\n" "Print me."  
 } 
 
 print_me
 
+# Capture exit status of command to retVal.
 retVal=$? 
 
-exit_status() { 
+# If retVal not 0, let us know.
+exit_status() {
   if [[ $retVal != 0 ]]; then
     printf "%s\n" "Something went wrong, homie."
   else
