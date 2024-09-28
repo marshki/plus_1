16f9e635e0dde747f8bf1c2910d8e6799dca56d7
marshki <marshall@morethantomorrow.org>
2024-09-28 14:27:36 -0400

    add

---
+++ functions/cross_platform/user_check.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/emv bash
+#
+# Prompt user for input until non-existent username is entered.
+
+# Send ID and standard error to /dev/null
+username_check() {
+  while true; do
+    read -r -p "Enter username to add and press [Enter]: " username
+    if id "$username" >/dev/null 2>&1; then
+    	printf "%s\n" "ERROR: $username already exists. Try again."
+    else
+      	printf "%s\n" "$username does not exist. Continuing..."
+      break
+    fi
+  done
+}
+
+username_check
