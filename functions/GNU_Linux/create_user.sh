b3a3ee101fde464e5f2e4c9dedb9993d0a477393
marshki <marshall@morethantomorrow.org>
2024-10-01 12:06:45 -0400

    fix comment error

--- functions/GNU_Linux/create_user.sh
+++ functions/GNU_Linux/create_user.sh
@@ -8,20 +8,22 @@ realname='Steve Jobs'
 # For reference: https://linux.die.net/man/8/useradd
 # TODO: add explicit description of this command. 
 
+#create_user() {
+#  printf "%s\\n" "Adding user..."
+#  useradd --create-home --user-group --home /home/"$username" \
+#  --comment "$realname" --shell /bin/bash "$username"
+#}
+
+#create_user
+
 create_user() {
-  printf "%s\\n" "Adding user..."
-  useradd --create-home --user-group --home /home/"$username" \
-  --comment "$realname" --shell /bin/bash "$username"
+    printf "%s\n" "Adding user..."
+    if useradd --create-home --user-group --home "/home/$username" --comment "$realname" --shell /bin/bash "$username"; then
+        printf "%s\n" "New user created: name='$username', home='/home/$username', shell='/bin/bash'"
+    else
+        printf "%s\n" "ERROR: Failed to create user $username"
+        exit 1
+    fi
 }
 
 create_user
-
-#create_user() {
-#    printf "%s\n" "Adding user..."
-#    if useradd --create-home --user-group --home "/home/$username" --comment "$realname" --shell /bin/bash "$username"; then
-#        log "New user created: name='$username', home='/home/$username', shell='/bin/bash'"
-#    else
-#        log "ERROR: Failed to create user $username"
-#        exit 1
-#    fi
-#}
