7a59217d8b1e39ae0692413f5c71b45d0221e19d
marshki <marshall@morethantomorrow.org>
2024-10-02 10:20:54 -0400

    update spacing, comments

--- functions/GNU_Linux/create_default_dirs.sh
+++ functions/GNU_Linux/create_default_dirs.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
+#
 # Create default user directories, e.g. Desktop, Music, etc.
-# For reference: 
+# in /home
+# For reference:
 # https://wiki.archlinux.org/index.php/XDG_user_directories
 
 username='sjobs'
 
 create_default_dirs() {
- 
   if [[ -n $(command -v xdg-user-dirs-update) ]]; then
     su ${username} -c xdg-user-dirs-update
-  # To use sudo instead of su, do:
-  # read -s -p "Enter sudo password: " passy
-  #
-  # printf "%s" "$passy" |sudo -u $username -i xdg-user-dirs-update
   fi
-}
-
+} 
 create_default_dirs
+
+# To use sudo instead of su, do:
+# read -s -p "Enter sudo password: " passy
+#
+# printf "%s" "$passy" |sudo -u $username -i xdg-user-dirs-update
