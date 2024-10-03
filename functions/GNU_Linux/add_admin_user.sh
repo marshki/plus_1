f899621e90f64b54f39b822d89590db1544460ff
marshki <marshall@morethantomorrow.org>
2024-10-03 11:19:55 -0400

    refactor & comment

--- functions/GNU_Linux/add_admin_user.sh
+++ functions/GNU_Linux/add_admin_user.sh
@@ -1,31 +1,29 @@
 #!/use/bin/env bash
+#
+# Grant user administrator privileges.
+username='sjobs'
+
 # Check OS for administrative group via `getent`,
 # then, using `usermod`, add user to group.
 # sudo = Debian-based
 # wheel = RHEL-based
 
-username='sjobs'
-
 add_admin_user() {
   read -r -p "Add user to administrator (sudo/wheel) group [yes/no]? " prompt
-
     if [[ "$prompt" == "yes" ]]; then
         printf "%s\n" "Checking for administrator group..."
-
         if getent group sudo >/dev/null; then
             if usermod --append --groups sudo "$username"; then
                 printf "%s\n" "User $username added to sudo group"
             else
                 printf "%s\n" "ERROR: Failed to add user $username to sudo group"
             fi
-
         elif getent group wheel >/dev/null; then
             if usermod --append --groups wheel "$username"; then
                 printf "%s\n" "User $username added to wheel group"
             else
                 printf "%s\n" "ERROR: Failed to add user $username to wheel group"
             fi
-
         else
             printf "%s\n" "ERROR: No admin group found. Exiting." >&2
             exit 1
