61a61b70abb9006067a4128b4c77ff111995bcde
marshki <marshall@morethantomorrow.org>
2024-10-03 11:44:50 -0400

    fix spacing

--- functions/GNU_Linux/add_admin_user.sh
+++ functions/GNU_Linux/add_admin_user.sh
@@ -7,28 +7,27 @@ username='sjobs'
 # then, using `usermod`, add user to group.
 # sudo = Debian-based
 # wheel = RHEL-based
-
 add_admin_user() {
   read -r -p "Add user to administrator (sudo/wheel) group [yes/no]? " prompt
     if [[ "$prompt" == "yes" ]]; then
-        printf "%s\n" "Checking for administrator group..."
-        if getent group sudo >/dev/null; then
-            if usermod --append --groups sudo "$username"; then
-                printf "%s\n" "User $username added to sudo group"
-            else
-                printf "%s\n" "ERROR: Failed to add user $username to sudo group"
-            fi
-        elif getent group wheel >/dev/null; then
-            if usermod --append --groups wheel "$username"; then
-                printf "%s\n" "User $username added to wheel group"
-            else
-                printf "%s\n" "ERROR: Failed to add user $username to wheel group"
-            fi
-        else
-            printf "%s\n" "ERROR: No admin group found. Exiting." >&2
-            exit 1
-        fi
-    fi
+      printf "%s\n" "Checking for administrator group..."
+      if getent group sudo >/dev/null; then
+          if usermod --append --groups sudo "$username"; then
+              printf "%s\n" "User $username added to sudo group"
+          else
+              printf "%s\n" "ERROR: Failed to add user $username to sudo group"
+          fi
+      elif getent group wheel >/dev/null; then
+          if usermod --append --groups wheel "$username"; then
+              printf "%s\n" "User $username added to wheel group"
+          else
+              printf "%s\n" "ERROR: Failed to add user $username to wheel group"
+          fi
+      else
+          printf "%s\n" "ERROR: No admin group found. Exiting." >&2
+          exit 1
+      fi
+  fi
 }
 
 add_admin_user
