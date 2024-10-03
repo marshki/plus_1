8d6605f3a794dded7f1e9e487e8fa1bf5463906e
marshki <marshall@morethantomorrow.org>
2024-10-03 11:50:05 -0400

    rm excess spacing

--- functions/GNU_Linux/create_user.sh
+++ functions/GNU_Linux/create_user.sh
@@ -12,14 +12,14 @@ realname='Steve Jobs'
 # --comment: users full name or description of account
 # --shell: define default login shell
 create_user() {
-    printf "%s\n" "Adding user..."
-    if useradd --create-home --user-group --home "/home/$username" \
-               --comment "$realname" --shell /bin/bash "$username"; then
-        printf "%s\n" "New user created: name='$username', home='/home/$username', shell='/bin/bash'"
-    else
-        printf "%s\n" "ERROR: Failed to create user $username"
-        exit 1
-    fi
+  printf "%s\n" "Adding user..."
+  if useradd --create-home --user-group --home "/home/$username" \
+             --comment "$realname" --shell /bin/bash "$username"; then
+    printf "%s\n" "New user created: name='$username', home='/home/$username', shell='/bin/bash'"
+  else
+    printf "%s\n" "ERROR: Failed to create user $username"
+    exit 1
+  fi
 }
 
 create_user
