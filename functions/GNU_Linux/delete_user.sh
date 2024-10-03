3818616e0e14b7cf2e733d33a9e799e2b4bf2f8d
marshki <marshall@morethantomorrow.org>
2024-10-03 09:28:30 -0400

    refactor

--- functions/GNU_Linux/delete_user.sh
+++ functions/GNU_Linux/delete_user.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
+#
 # Delete user account in GNU/Linux.
 
 username='tcook'
 
 delete_user () {
   printf "%s\n" "Deleting user..." 
-
   deluser --remove-home $username
+  # alternative:
   # userdel --remove --force $username  
-} 
+}
+
+delete_user
