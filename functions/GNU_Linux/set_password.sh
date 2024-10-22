7912f28fb73bcc57c2464930c9be8ae8b921102a
marshki <marshall@morethantomorrow.org>
2024-10-22 12:57:00 -0400

    add full path to chpasswd binary

--- functions/GNU_Linux/set_password.sh
+++ functions/GNU_Linux/set_password.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 #
 # Create password for user.
+chpasswd="usr/sbin/chpasswd"
 
 username='sjobs'
 passy='1morething'
 
 set_password() {
   printf "%s\\n" "Setting password..."
-  printf "%s" "$username:$passy" | chpasswd
+  printf "%s" "$username:$passy" | $chpasswd
 }
 
 set_password
