3d99db764468d8a093ef2b4213acbc9a53424d51
marshki <marshall@morethantomorrow.org>
2024-11-12 09:19:53 -0500

    address spacing

--- src/macOS/macOS_add.sh
+++ src/macOS/macOS_add.sh
@@ -13,15 +13,11 @@ LOG_FILE="macOS_add.log"
 
 # LOG
 # Write changes w/ timestamp to LOG_FILE for tracking.
-
 log() {
   printf "%s\n" "$(date +"%b %d %X") $*" | tee -a "$LOG_FILE"
 }
 
-# macOS_add
-
 # Is current UID 0? If not, exit.
-
 root_check() {
   if [ "$EUID" -ne 0 ]; then
     log "ERROR: Root privileges required to continue. Exiting." >&2
@@ -30,14 +26,11 @@ root_check() {
 }
 
 # Username prompt.
-
 get_username() {
   while true; do
     read -r -p "Enter username to add and press [Enter]: " username
-
     if id "$username" >/dev/null 2>&1; then
       log "ERROR: $username already exists. Try again."
-
     else
       printf "%s\n" "$username does not exist. Continuing..."
       break
@@ -46,20 +39,17 @@ get_username() {
 }
 
 # Get highest current UID and increment +1.
-
 get_uid() {
   uid=$(dscl . -list /Users UniqueID | sort --numeric-sort --key=2 | awk 'END{print $2}')
   increment_uid=$((uid + 1))
 }
 
 # Real name prompt.
-
 get_realname() {
   read -rp "Enter 'real name' to add and press [Enter]: " realname
 }
 
 # Primary group ID prompt.
-
 get_primarygroup() {
   printf "%s\n" "Primary Group ID: 80=admin, 20=standard"
   read -rp "Enter primary group ID to add and press [Enter]: " primarygroup
@@ -72,7 +62,6 @@ get_primarygroup() {
 #}
 
 # Password prompt.
-
 get_password() {
   while true; do
     read -r -s -p "Enter password to add and press [Enter]: " pass1
@@ -82,7 +71,6 @@ get_password() {
 
     if [[ "$pass1" != "$pass2" ]]; then
       log "ERROR: Passwords do not match."
-
     else
       log "Passwords match. Continuing..."
       break
@@ -91,7 +79,6 @@ get_password() {
 }
 
 # User info wrapper.
-
 user_info() {
   get_uid
   get_username
@@ -102,7 +89,6 @@ user_info() {
 }
 
 # Create account via dscl using input from user_info.
-
 create_user() {
   printf "%s\n" "Adding user..."
 
@@ -133,11 +119,9 @@ create_account() {
 }
 
 # Exit status check.
-
 exit_status() {
   if [[ $1 -ne 0 ]]; then
     log "Something went wrong, homie."
-
   else
     log "Done."
     printf "%s\n" "User account created successfully."
@@ -146,16 +130,13 @@ exit_status() {
 
 main() {
   root_check
-
   printf "%s\n" "plus_1: A Bash script to create local user accounts in macOS."
 
   while true; do
     read -r -p "Create user account? (yes/no): " answer
-
     if [[ "${answer}" = "yes" ]]; then
       printf "%s\n" "Let's add a user..."
       create_account
-
     else
       printf "%s\n" "Exiting."
       exit 0
