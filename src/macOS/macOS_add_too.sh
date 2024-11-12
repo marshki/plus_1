5d2818b74bc38f40126d53f8c25c1cea2f82a526
marshki <marshall@morethantomorrow.org>
2024-11-12 11:10:04 -0500

    update, remove excess spacing

--- src/macOS/macOS_add_too.sh
+++ src/macOS/macOS_add_too.sh
@@ -18,8 +18,6 @@ log() {
   printf "%s\n" "$(date +"%b %d %X") $*" | tee -a "$LOG_FILE"
 }
 
-# macOS_add_too
-
 # Is current UID 0? If not, exit.
 
 root_check() {
@@ -30,14 +28,11 @@ root_check() {
 }
 
 # Username prompt.
-
 get_username() {
   while true; do
     read -r -p "Enter username to add and press [Enter]: " username
-
     if id "$username" >/dev/null 2>&1; then
       log "ERROR: $username already exists. Try again."
-
     else
       printf "%s\n" "$username does not exist. Continuing..."
       break
@@ -46,7 +41,6 @@ get_username() {
 }
 
 # Get highest current UID and increment +1.
-
 get_uid() {
   uid=$(getent passwd | awk -F: '{print $3}' | sort -n | tail -1)
   increment_uid=$((uid + 1))
@@ -79,10 +73,8 @@ get_password() {
     printf "\n"
     read -r -s -p "Re-enter password to add and press [Enter]: " pass2
     printf "\n"
-
     if [[ "$pass1" != "$pass2" ]]; then
       log "ERROR: Passwords do not match."
-
     else
       log "Passwords match. Continuing..."
       break
@@ -91,21 +83,18 @@ get_password() {
 }
 
 # User info wrapper.
-
 user_info() {
   get_uid
   get_username
   get_realname
   get_primarygroup
-  #get_hint
+  get_hint
   get_password
 }
 
 # Create account via sysadminctl using input from user_info.
-
 create_user() {
   printf "%s\n" "Adding user..."
-
   sysadminctl -addUser "$username" \
               -UID "$increment_uid" \
 	      -fullName "$realname" \
@@ -114,19 +103,16 @@ create_user() {
 	      -home /Users/"$username" \
               -shell /bin/bash \
 	      -group "$primarygroup"
-
   log "New user created: name='$username', home=/Users/'$username', shell=/bin/bash"
 }
 
 # Create account function.
-
 create_account() {
   user_info
   create_user
 }
 
 # Exit status check.
-
 exit_status() {
   if [[ $1 -ne 0 ]]; then
     log "Something went wrong, homie."
@@ -139,23 +125,18 @@ exit_status() {
 
 main() {
   root_check
-
   printf "%s\n" "plus_1: A Bash script to create local user accounts in macOS."
-
   while true; do
     read -r -p "Create user account? (yes/no): " answer
-
     if [ "$answer" = yes ]; then
       printf "%s\n" "Let's add a user..."
       create_account
-
     else
       printf "%s\n" "Exiting."
       exit 0
     fi
   done
 }
-
 main "$@"
 
 retVal=$?
