09dcc7e213fa85ae53fc0f6a7f1ef45a07988ba3
marshki <marshall@morethantomorrow.org>
2024-11-12 12:13:12 -0500

    update

--- functions/macOS/get_uid.sh
+++ functions/macOS/get_uid.sh
@@ -4,6 +4,14 @@
 # pipe to awk to print last record from second field
 # increment +1
 
+# getent deprecated in newwer versions of macOS.
+# This is for historical purposes.
+# Get highest current UID and increment +1.
+#get_uid() {
+#  uid=$(getent passwd | awk -F: '{print $3}' | sort -n | tail -1)
+#  increment_uid=$((uid + 1))
+#}
+
 get_uid() {
   uid=$(dscl . -list /Users UniqueID |sort --numeric-sort --key=2 |awk 'END{print $2}')
   printf "%s\n" "Highest UID: $uid"
